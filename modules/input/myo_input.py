e218616919fd3d707dd8c12dbbdf4094665cad87
Jeet Gajjar <j211668@gmail.com>
2018-02-10 21:12:54 -0500

    refactored myo; need to move quatToEuler formula

--- modules/input/myo_input.py
+++ modules/input/myo_input.py
@@ -1,17 +1,55 @@
 from threading import Thread
-
+from myo import init, Hub, Feed
+from decimal import *
 from observable import Observable
+import math
 
 obs = Observable()
+
+getcontext().prec = 4
+
+init('../../sdk/myo.framework')
+feed = Feed()
+hub = Hub()
+hub.run(1000, feed)
 
 def getObservable():
     print("got observable")
     return obs
 
+def quaternionToEuler(w, x, y, z):
+    ysqr = y * y
+
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + ysqr)
+    X = math.degrees(math.atan2(t0, t1))
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    Y = math.degrees(math.asin(t2))
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (ysqr + z * z)
+    Z = math.degrees(math.atan2(t3, t4))
+
+    return [format(X, '2.3f'), format(Y, '2.3f'), format(Z, '2.3f')]
+
 def main():
     #Repeating logic goes in here
     while True:
-        obs.trigger('input',{ 'myo': {} })
+        try:
+            myo = feed.wait_for_single_device(timeout=2.0)  # connecting to Myo
+            if not myo:
+                print("No Myo connected after 2 seconds")
+            print("Connected to Myo")
+            while hub.running and myo.connected:
+                quat = myo.orientation
+
+        finally:
+            hub.shutdown()  #!! crucial
+
+        obs.trigger('input', {'myo': quat})
         return
 
 main_thread = Thread(target=main)
