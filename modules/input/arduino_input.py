59cdc522e0be63631eaa90814b7da61af64a3901
Jeet Gajjar <j211668@gmail.com>
2018-02-11 01:30:14 -0500

    soft_pot works

--- modules/input/arduino_input.py
+++ modules/input/arduino_input.py
@@ -1,5 +1,7 @@
 from threading import Thread
 
+import serial
+import time
 from observable import Observable
 
 obs = Observable()
@@ -8,12 +10,24 @@ def getObservable():
     print("got observable")
     return obs
 
-def main():
-    #Repeating logic goes in here
-    while True:
-        obs.trigger('input',{ 'soft_pot': 0 })
-        return
+connected = False
 
-main_thread = Thread(target=main)
-main_thread.setDaemon(True)
-main_thread.start()
+serial_port = serial.Serial('/dev/cu.usbmodem14111', timeout=2, baudrate=115200)
+time.sleep(2)
+
+def trigger(data_str):
+    obs.trigger('input',{'soft_pot': data_str})
+
+def read_from_port(ser):
+    global connected
+    while not connected:
+        #serin = ser.read()
+        connected = True
+
+        while True:
+            if (ser.inWaiting() > 0):
+                data_str = ser.readline(ser.inWaiting()).decode('ascii')
+                trigger(data_str)
+
+thread = Thread(target=read_from_port, args=(serial_port,))
+thread.start()
