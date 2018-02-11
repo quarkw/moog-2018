4fda9a788cb0aba19bbc884fc75e57725b32b8e2
Quark Wei <quark.j.wei@gmail.com>
2018-02-10 21:46:10 -0500

    Add enums

---
+++ modules/enums/interval.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class Interval(Enum):
+    TONIC = 0
+    MINOR2 = 5
+    MAJOR2 = 10
+    MINOR3 = 15
+    MAJOR3 = 20
+    FOURTH = 25
+    TRITONE = 30
+    FIFTH = 35
+    MINOR6 = 40
+    MAJOR6 = 45
+    MINOR7 = 50
+    MAJOR7 = 55
+    OCTAVE = 60
+    OCTAVE2 = 120
---
+++ modules/enums/note.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Note(Enum):
+    W = 4
+    H = 2
+    Q = 1
+    QT = 2/3
+    E = 1/2
+    ET = 1/3
+    SX = 1/4
+    SXT = 1/6
+    TH = 1/8
+    SXF = 1/16
